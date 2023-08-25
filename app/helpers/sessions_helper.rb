487a32c56a20fe33ea65ef7f575078995abb4102
Niels Bertling <niels.bertling@finstreet.de>
2023-08-25 11:22:26 +0200

    Finish log in / log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+
+  end
+
   # Returns the current logged-in user (if any)
   def current_user
     # if @current_user.nil?
@@ -12,19 +20,34 @@ module SessionsHelper
     # else 
     #   @current_user
     # end
-
+    
     # @current_user = @current_user || User.find_by(session[:user_id]) 
-    @current_user ||= User.find_by(id: session[:user_id]) 
-
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
-  #Returns true if the user is logged in, false otherwise
+  # Returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
   end
 
+  #Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Log sout the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
