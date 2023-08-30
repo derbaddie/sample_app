98a76f768a76d77f590e8b08321d0a9b09717c2b
Niels Bertling <niels.bertling@finstreet.de>
2023-08-30 11:14:44 +0200

    fix bugs with ':see_other'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -49,7 +49,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
-    assert_response :see_other
     assert_redirected_to login_url
   end
 
@@ -58,7 +57,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
-    assert_response :see_other
     assert_redirected_to root_url
   end
 
